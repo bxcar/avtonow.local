1daead86e5f69a261aa21e343904c78fbde8b713
bxcar <malanchukdima@mail.ru>
2018-06-19 11:14:52 +0300

    catalog done

--- wp-content/themes/avtonow/functions.php
+++ wp-content/themes/avtonow/functions.php
@@ -7,82 +7,82 @@
  * @package avtonow
  */
 
-if ( ! function_exists( 'avtonow_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function avtonow_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on avtonow, use a find and replace
-		 * to change 'avtonow' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'avtonow', get_template_directory() . '/languages' );
+if (!function_exists('avtonow_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function avtonow_setup() {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on avtonow, use a find and replace
+         * to change 'avtonow' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('avtonow', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'avtonow' ),
-			'menu-2' => esc_html__( 'Sidebar-menu', 'avtonow' ),
-		) );
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(array(
+            'menu-1' => esc_html__('Primary', 'avtonow'),
+            'menu-2' => esc_html__('Sidebar-menu', 'avtonow'),
+        ));
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'avtonow_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+        // Set up the WordPress core custom background feature.
+        add_theme_support('custom-background', apply_filters('avtonow_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        )));
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 250,
+            'width' => 250,
+            'flex-width' => true,
+            'flex-height' => true,
+        ));
+    }
 endif;
-add_action( 'after_setup_theme', 'avtonow_setup' );
+add_action('after_setup_theme', 'avtonow_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -92,12 +92,13 @@ add_action( 'after_setup_theme', 'avtonow_setup' );
  * @global int $content_width
  */
 function avtonow_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'avtonow_content_width', 640 );
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+    $GLOBALS['content_width'] = apply_filters('avtonow_content_width', 640);
 }
-add_action( 'after_setup_theme', 'avtonow_content_width', 0 );
+
+add_action('after_setup_theme', 'avtonow_content_width', 0);
 
 /**
  * Register widget area.
@@ -105,33 +106,35 @@ add_action( 'after_setup_theme', 'avtonow_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function avtonow_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'avtonow' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'avtonow' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar', 'avtonow'),
+        'id' => 'sidebar-1',
+        'description' => esc_html__('Add widgets here.', 'avtonow'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
-add_action( 'widgets_init', 'avtonow_widgets_init' );
+
+add_action('widgets_init', 'avtonow_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
 function avtonow_scripts() {
-	wp_enqueue_style( 'avtonow-style', get_stylesheet_uri() );
+    wp_enqueue_style('avtonow-style', get_stylesheet_uri());
 
 //	wp_enqueue_script( 'avtonow-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 //	wp_enqueue_script( 'avtonow-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'avtonow_scripts' );
+
+add_action('wp_enqueue_scripts', 'avtonow_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -183,4 +186,36 @@ if (function_exists('acf_add_options_page')) {
         'menu_slug' => 'footer',
         'parent_slug' => 'theme-general-settings',
     ));
+}
+
+function custom_admin_js() {
+    echo '"<style>
+                #categorydiv,
+                #postimagediv {  
+                    display: block;
+                }
+           </style>"';
+}
+
+add_action('admin_footer', 'custom_admin_js');
+
+add_filter('navigation_markup_template', 'my_navigation_template', 10, 2);
+function my_navigation_template($template, $class) {
+    /*
+    Вид базового шаблона:
+    <nav class="navigation %1$s" role="navigation">
+        <h2 class="screen-reader-text">%2$s</h2>
+        <div class="nav-links">%3$s</div>
+    </nav>
+    */
+//    $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    global $wp;
+//    echo home_url( $wp->request );
+
+    return '
+	<div class="catalog-content__pagination pagination %1$s" role="navigation">
+		%3$s
+		<a class="pagination__show-all" href="' . home_url( $wp->request ) . '?showall=1">показать все</a>
+	</div>    
+	';
 }
